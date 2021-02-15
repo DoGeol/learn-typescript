d017b2b9f3ced3c2d843cc8c2d5f88374f5f55f2
편도걸 <pdg2491@naver.com>
2021-02-16 00:03:40 +0900

    [Study] 1. 타입스크립트 사용하는 이유
    - sample.js / ts를 통해 같은 함수를 js와 ts로 작성 했을 때 차이를 확인

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,13 +9,42 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} city
+ * @property {string} street
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function(response) {
+  response.address.street
+})
+
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user)
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.addres.street;
     })
     .catch(function (error) {
       console.log(error);
